d66c788f1a0dcf256df1d11a08305428cd26416a
laradiba <l.dibattista@studenti.poliba.it>
2026-02-23 19:59:51 +0100

    Add movie data loading and Optuna tuning

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from pathlib import Path
 from baseline import run_baseline
 from matrix_factorization import run_mf
 from user_based_cf import run_usercf
-
+from tuning import run_optuna
 
 # =========================
 # 1) LOAD DATA
 # =========================
 DATA_DIR = Path("data/ml-1m")
 ratings_path = DATA_DIR / "ratings.dat"
+movies_path = DATA_DIR / "movies.dat"
 
 ratings = pd.read_csv(
     ratings_path,
@@ -22,8 +23,15 @@ ratings = pd.read_csv(
     names=["userId", "movieId", "rating", "timestamp"]
 )
 
-ratings = ratings.sample(frac=1, random_state=42).reset_index(drop=True)
+movies = pd.read_csv(
+    movies_path,
+    sep="::",
+    engine="python",
+    names=["movieId", "title", "genres"],
+    encoding="latin-1"
+)
 
+ratings = ratings.sample(frac=1, random_state=42).reset_index(drop=True)
 
 # =========================
 # 2) SPLIT (1 test + 1 val per utente)
@@ -51,7 +59,6 @@ print("Train:", train.shape)
 print("Val:", val.shape)
 print("Test:", test.shape)
 
-
 # =========================
 # 3) RUN MODELS
 # =========================
@@ -60,7 +67,10 @@ print("\n===== BASELINE =====")
 run_baseline(train, val, test)
 
 print("\n===== MATRIX FACTORIZATION =====")
-run_mf(train, val, test)
+run_mf(train, val, test, ratings, movies)
 
 print("\n===== USER-BASED CF =====")
 run_usercf(train, val, test)
+
+print("\n===== OPTUNA TUNING (MF) =====")
+run_optuna(train, val, test, ratings, movies, n_trials=20)
